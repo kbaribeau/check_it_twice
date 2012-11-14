4c9982a3332d1cc809176b6f23bff81dd5281d4b
Kevin Baribeau <kevin.baribeau@gmail.com>
2012-11-14 00:35:56 -0700

    initial commit

---
+++ pyXmas.py
@@ -0,0 +1,15 @@
+import random
+
+def xmas(family):
+	matches = []
+	for giver in family:
+		matches.append((giver, family[(family.index(giver)+1) % (len(family))]))
+	return matches
+
+
+if __name__ =='__main__':
+	family = ['Mike', 'Muriel', 'Steph', 'James', 'Kevin', 'Laurier', 'Cameron', 'Jenelle']
+	random.shuffle(family)
+	matches =  xmas(family)
+	for (x,y) in matches:
+		print "%s gives to %s" % (x, y)
