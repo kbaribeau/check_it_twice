45b110fde78ad2768e263b1a980db58a6282078c
Kevin Baribeau <kevin.baribeau@gmail.com>
2018-12-04 16:38:45 -0600

    Formatting

--- pyXmas.py
+++ pyXmas.py
@@ -1,15 +1,14 @@
 import random
 
 def xmas(family):
-	matches = []
-	for giver in family:
-		matches.append((giver, family[(family.index(giver)+1) % (len(family))]))
-	return matches
-
+  matches = []
+  for giver in family:
+    matches.append((giver, family[(family.index(giver)+1) % (len(family))]))
+  return matches
 
 if __name__ =='__main__':
-	family = ['Mike', 'Muriel', 'Steph', 'James', 'Kevin', 'Laurier', 'Cameron']
-	random.shuffle(family)
-	matches =  xmas(family)
-	for (x,y) in matches:
-		print "%s gives to %s" % (x, y)
+  family = ['Mike', 'Muriel', 'Steph', 'James', 'Kevin', 'Laurier', 'Cameron']
+  random.shuffle(family)
+  matches = xmas(family)
+  for (x,y) in matches:
+    print "%s gives to %s" % (x, y)
